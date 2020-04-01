a280b82e096eb5f68f2bcc8eadbdb76f949a37df
WaraleeS <waralee.s@wisesight.com>
2020-04-01 14:02:37 +0700

    api post update articles

--- main.go
+++ main.go
@@ -24,19 +24,19 @@ type User struct {
 func main() {
 	articles := map[int]Article{
 		1: {
-			ID:      11,
+			ID:      1,
 			Title:   "Thailand",
 			Content: "this is thailand content",
 			User_ID: "11111",
 		},
 		2: {
-			ID:      22,
+			ID:      2,
 			Title:   "US",
 			Content: "this is US content",
 			User_ID: "222222",
 		},
 		3: {
-			ID:      33,
+			ID:      3,
 			Title:   "Germany",
 			Content: "this is Germany content",
 			User_ID: "33333",
@@ -49,6 +49,7 @@ func main() {
 		return c.JSON(http.StatusOK, map[string]interface{}{"message": "halo gorgeous"})
 		// return c.String(http.StatusOK, fmt.Sprint("halo gorgeous :)"))
 	})
+
 	e.GET("/articles", func(c echo.Context) error {
 		// article := &Article{
 		// 	ID:      1,
@@ -58,32 +59,59 @@ func main() {
 		// }
 		// return c.JSON(http.StatusOK, article)
 
-		list := []Article{}
+		listArticle := []Article{}
 		for _, val := range articles {
-			list = append(list, val)
+			listArticle = append(listArticle, val)
 		}
-		return c.JSON(http.StatusOK, list)
+		return c.JSON(http.StatusOK, listArticle)
 
 	})
+
 	e.GET("/articles/:id", func(c echo.Context) error {
 		id := c.Param("id")
-		newID, err := strconv.Atoi(id)
+		intID, err := strconv.Atoi(id)
+		if err != nil {
+			return c.JSON(http.StatusOK, map[string]interface{}{"message": "sorry girl, Error Response"})
+		}
+		listArticle, true := articles[intID]
+		if !true {
+			return c.JSON(http.StatusOK, map[string]interface{}{"message": "sorry girl, Error Response"})
+		}
+		return c.JSON(http.StatusOK, listArticle)
+	})
+
+	e.POST("/articles", func(c echo.Context) error {
+		article := Article{}
+		maxKeyID := 0
+		err := c.Bind(&article)
 		if err != nil {
 			return err
 		}
-		article := &Article{
-			ID:      newID,
-			Title:   "Annyoeng@Haseyo.com",
-			Content: "Covid-19",
-			User_ID: "12345678",
+		for key := range articles {
+			if key > maxKeyID {
+				maxKeyID = key
+			}
+		}
+		nextKeyID := maxKeyID + 1
+		article.ID = nextKeyID
+		articles[nextKeyID] = article
+
+		article = Article{
+			ID:      nextKeyID,
+			Title:   "Italy",
+			Content: "This is Italy content",
+			User_ID: "44444",
 		}
 		return c.JSON(http.StatusOK, article)
 	})
-	e.POST("/articles", func(c echo.Context) error {
-		return c.String(http.StatusOK, fmt.Sprintf("insert article"))
-	})
 	e.POST("/articles/:id", func(c echo.Context) error {
-		return c.String(http.StatusOK, fmt.Sprintf("update article"))
+		article := Article{}
+		err := c.Bind(&article)
+		if err != nil {
+			return err
+		}
+		articles[article.ID] = article
+		return c.JSON(http.StatusOK, article)
 	})
 	e.DELETE("/articles/:id", func(c echo.Context) error {
 		return c.String(http.StatusOK, fmt.Sprintf("update article"))
