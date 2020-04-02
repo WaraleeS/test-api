e2b884e085fbb4185b8cb3afed401560ab3dbc24
WaraleeS <waralee.s@wisesight.com>
2020-04-02 17:48:45 +0700

    sql database article api with go

--- main.go
+++ main.go
@@ -2,21 +2,24 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"net/http"
+	"strconv"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 type Article struct {
-	ID       int       `json:"id"`
-	Title    string    `json:"title"`
-	Content  string    `json:"content"`
-	UserID   int       `json:"user_id"`
-	CreateAt time.Time `json:"create_at"`
-	UpdateAt time.Time `json:"update_at"`
-	DeleteAt time.Time `json:"delete_at"`
+	ID        int       `json:"id"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	UserID    int       `json:"user_id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at"`
 }
 
 func main() {
@@ -48,6 +51,7 @@ func main() {
 	}
 
 	e := echo.New()
+	e.Use(middleware.Logger())
 
 	e.GET("/", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, map[string]interface{}{"message": "halo gorgeous"})
@@ -56,15 +60,15 @@ func main() {
 
 	e.GET("/articles", func(c echo.Context) error {
 		listArticle := []Article{}
-		var a Article
 
-		statement := "SELECT id, title, content, user_id, create_at, update_at delete_at FROM articles"
+		statement := "SELECT id, title, content, user_id, created_at, updated_at, deleted_at FROM articles"
 		results, err := db.Query(statement)
 		if err != nil {
 			return err
 		}
 		for results.Next() {
-			results.Scan(&a.ID, &a.Title, &a.Content, &a.UserID, &a.CreateAt, &a.UpdateAt, &a.DeleteAt)
+			a := Article{}
+			results.Scan(&a.ID, &a.Title, &a.Content, &a.UserID, &a.CreatedAt, &a.UpdatedAt, &a.DeletedAt)
 			listArticle = append(listArticle, a)
 		}
 
@@ -74,55 +78,123 @@ func main() {
 		return c.JSON(http.StatusOK, listArticle)
 	})
 
-	// e.GET("/articles/:id", func(c echo.Context) error {
-	// 	id := c.Param("id")
-	// 	intID, err := strconv.Atoi(id)
-	// 	if err != nil {
-	// 		return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "sorry girl, Error Response"})
-	// 	}
-	// 	article, true := articles[intID]
-	// 	if !true {
-	// 		return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "sorry girl, Error Response"})
-	// 	}
-	// 	return c.JSON(http.StatusOK, article)
-	// })
+	e.GET("/articles/:id", func(c echo.Context) error {
+		// id := c.Param("id")
+		// intID, err := strconv.Atoi(id)
+		// if err != nil {
+		// 	return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "sorry girl, Error Response"})
+		// }
+		// article, true := articles[intID]
+		// if !true {
+		// 	return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "sorry girl, Error Response"})
+		// }
+		// return c.JSON(http.StatusOK, article)
 
-	// e.POST("/articles", func(c echo.Context) error {
-	// 	article := Article{}
-	// 	max := 0
-	// 	err := c.Bind(&article)
-	// 	if err != nil {
-	// 		return err
-	// 	}
-	// 	for key := range articles {
-	// 		if key > max {
-	// 			max = key
-	// 		}
-	// 	}
-	// 	nextKey := max + 1
-	// 	article.ID = nextKey
-	// 	articles[nextKey] = article
+		id := c.Param("id")
+		intID, err := strconv.Atoi(id)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "Error select database !!"})
+		}
+		a := Article{}
+		statement := "SELECT * FROM articles WHERE id = ?"
+		result := db.QueryRow(statement, intID)
+		result.Scan(&a.ID, &a.Title, &a.Content, &a.UserID, &a.CreatedAt, &a.UpdatedAt, &a.DeletedAt)
+		return c.JSON(http.StatusOK, a)
+	})
 
-	// 	return c.JSON(http.StatusOK, article)
-	// })
-	// e.POST("/articles/:id", func(c echo.Context) error {
-	// 	article := Article{}
-	// 	err := c.Bind(&article)
-	// 	if err != nil {
-	// 		return err
-	// 	}
-	// 	articles[article.ID] = article
-	// 	return c.JSON(http.StatusOK, article)
-	// })
-	// e.DELETE("/articles/:id", func(c echo.Context) error {
-	// 	id := c.Param("id")
-	// 	intID, err := strconv.Atoi(id)
-	// 	if err != nil {
-	// 		return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "sorry girl, Error Response"})
-	// 	}
-	// 	delete(articles, intID)
-	// 	return c.JSON(http.StatusOK, map[string]interface{}{"message": "article deleted!"})
-	// })
+	e.POST("/articles", func(c echo.Context) error {
+		a := Article{}
+		statement, err := db.Prepare("INSERT INTO articles (title, content, user_id) VALUES(?, ?, ?)")
+		if err != nil {
+			return err
+		}
+		res, err := statement.Exec(a.Title, a.Content, a.UserID)
+		if err != nil {
+			return err
+		}
+		lastID, err := res.LastInsertId()
+		if err != nil {
+			return err
+		}
+		a.ID = int(lastID)
+		return c.JSON(http.StatusOK, a)
+
+		// article := Article{}
+		// max := 0
+		// err := c.Bind(&article)
+		// if err != nil {
+		// 	return err
+		// }
+		// for key := range articles {
+		// 	if key > max {
+		// 		max = key
+		// 	}
+		// }
+		// nextKey := max + 1
+		// article.ID = nextKey
+		// articles[nextKey] = article
+		// return c.JSON(http.StatusOK, article)
+	})
+
+	e.POST("/articles/:id", func(c echo.Context) error {
+		a := Article{}
+		id := c.Param("id")
+		err = c.Bind(&a)
+		if err != nil {
+			return err
+		}
+		statement, err := db.Prepare("UPDATE articles SET title=?, content=?, user_id=? WHERE id=?")
+		if err != nil {
+			return err
+		}
+		_, err = statement.Exec(a.Title, a.Content, a.UserID, id)
+		if err != nil {
+			fmt.Print(err)
+			return err
+		}
+		return c.JSON(http.StatusOK, a)
+
+		// article := Article{}
+		// err := c.Bind(&article)
+		// if err != nil {
+		// 	return err
+		// }
+		// articles[article.ID] = article
+		// return c.JSON(http.StatusOK, article)
+	})
+
+	e.DELETE("/articles/:id", func(c echo.Context) error {
+		// id := c.Param("id")
+		// intID, err := strconv.Atoi(id)
+		// if err != nil {
+		// 	return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "sorry girl, Error Response"})
+		// }
+		// statement := "DELETE FROM articles WHERE id = ?"
+		// _, err = db.Exec(statement, intID)
+		// if err != nil {
+		// 	return err
+		// }
+		// return c.JSON(http.StatusOK, map[string]interface{}{"message": "article deleted!"})
+
+		id := c.Param("id")
+		statement, err := db.Prepare("UPDATE articles SET deleted_at=NOW() where id=?")
+		if err != nil {
+			return err
+		}
+		_, err = statement.Exec(id)
+		if err != nil {
+			return err
+		}
+		return c.JSON(http.StatusOK, map[string]interface{}{"message": "Delete Successful !!"})
+
+		// id := c.Param("id")
+		// intID, err := strconv.Atoi(id)
+		// if err != nil {
+		// 	return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "sorry girl, Error Response"})
+		// }
+		// delete(articles, intID)
+		// return c.JSON(http.StatusOK, map[string]interface{}{"message": "article deleted!"})
+	})
 
 	e.Logger.Fatal(e.Start(":1234"))
 }
