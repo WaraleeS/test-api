13fe13f735b5a51f5ed97dc172f780cafeb6482b
WaraleeS <waralee.s@wisesight.com>
2020-04-02 11:56:24 +0700

    connect databace api articles to select articles

--- main.go
+++ main.go
@@ -1,45 +1,50 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
-	"strconv"
+	"time"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/labstack/echo/v4"
 )
 
 type Article struct {
-	ID      int    `json:"id"`
-	Title   string `json:"title"`
-	Content string `json:"content"`
-	User_ID string `json:"user_id"`
-}
-
-type User struct {
-	User_ID  string `json:"user_id"`
-	Username string `json:"username"`
-	Email    string `json:"email"`
+	ID       int       `json:"id"`
+	Title    string    `json:"title"`
+	Content  string    `json:"content"`
+	UserID   int       `json:"user_id"`
+	CreateAt time.Time `json:"create_at"`
+	UpdateAt time.Time `json:"update_at"`
+	DeleteAt time.Time `json:"delete_at"`
 }
 
 func main() {
-	articles := map[int]Article{
-		1: {
-			ID:      1,
-			Title:   "Thailand",
-			Content: "this is thailand content",
-			User_ID: "11111",
-		},
-		2: {
-			ID:      2,
-			Title:   "US",
-			Content: "this is US content",
-			User_ID: "222222",
-		},
-		3: {
-			ID:      3,
-			Title:   "Germany",
-			Content: "this is Germany content",
-			User_ID: "33333",
-		},
+	// articles := map[int]Article{
+	// 	1: {
+	// 		ID:      1,
+	// 		Title:   "Thailand",
+	// 		Content: "this is thailand content",
+	// 		UserID:  11111,
+	// 	},
+	// 	2: {
+	// 		ID:      2,
+	// 		Title:   "US",
+	// 		Content: "this is US content",
+	// 		UserID:  222222,
+	// 	},
+	// 	3: {
+	// 		ID:      3,
+	// 		Title:   "Germany",
+	// 		Content: "this is Germany content",
+	// 		UserID:  33333,
+	// 	},
+	// }
+
+	// connect database
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3307)/zocialeye")
+	if err != nil {
+		panic(err)
 	}
 
 	e := echo.New()
@@ -50,70 +55,74 @@ func main() {
 	})
 
 	e.GET("/articles", func(c echo.Context) error {
-		// article := &Article{
-		// 	ID:      1,
-		// 	Title:   "Annyoeng@Haseyo.com",
-		// 	Content: "Covid-19",
-		// 	User_ID: "12345678",
-		// }
-		// return c.JSON(http.StatusOK, article)
-
 		listArticle := []Article{}
-		for _, val := range articles {
-			listArticle = append(listArticle, val)
+		var a Article
+
+		statement := "SELECT id, title, content, user_id, create_at, update_at delete_at FROM articles"
+		results, err := db.Query(statement)
+		if err != nil {
+			return err
 		}
+		for results.Next() {
+			results.Scan(&a.ID, &a.Title, &a.Content, &a.UserID, &a.CreateAt, &a.UpdateAt, &a.DeleteAt)
+			listArticle = append(listArticle, a)
+		}
+
+		// for _, val := range articles {
+		// 	listArticle = append(listArticle, val)
+		// }
 		return c.JSON(http.StatusOK, listArticle)
 	})
 
-	e.GET("/articles/:id", func(c echo.Context) error {
-		id := c.Param("id")
-		intID, err := strconv.Atoi(id)
-		if err != nil {
-			return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "sorry girl, Error Response"})
-		}
-		article, true := articles[intID]
-		if !true {
-			return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "sorry girl, Error Response"})
-		}
-		return c.JSON(http.StatusOK, article)
-	})
+	// e.GET("/articles/:id", func(c echo.Context) error {
+	// 	id := c.Param("id")
+	// 	intID, err := strconv.Atoi(id)
+	// 	if err != nil {
+	// 		return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "sorry girl, Error Response"})
+	// 	}
+	// 	article, true := articles[intID]
+	// 	if !true {
+	// 		return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "sorry girl, Error Response"})
+	// 	}
+	// 	return c.JSON(http.StatusOK, article)
+	// })
 
-	e.POST("/articles", func(c echo.Context) error {
-		article := Article{}
-		max := 0
-		err := c.Bind(&article)
-		if err != nil {
-			return err
-		}
-		for key := range articles {
-			if key > max {
-				max = key
-			}
-		}
-		nextKey := max + 1
-		article.ID = nextKey
-		articles[nextKey] = article
+	// e.POST("/articles", func(c echo.Context) error {
+	// 	article := Article{}
+	// 	max := 0
+	// 	err := c.Bind(&article)
+	// 	if err != nil {
+	// 		return err
+	// 	}
+	// 	for key := range articles {
+	// 		if key > max {
+	// 			max = key
+	// 		}
+	// 	}
+	// 	nextKey := max + 1
+	// 	article.ID = nextKey
+	// 	articles[nextKey] = article
 
-		return c.JSON(http.StatusOK, article)
-	})
-	e.POST("/articles/:id", func(c echo.Context) error {
-		article := Article{}
-		err := c.Bind(&article)
-		if err != nil {
-			return err
-		}
-		articles[article.ID] = article
-		return c.JSON(http.StatusOK, article)
-	})
-	e.DELETE("/articles/:id", func(c echo.Context) error {
-		id := c.Param("id")
-		intID, err := strconv.Atoi(id)
-		if err != nil {
-			return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "sorry girl, Error Response"})
-		}
-		delete(articles, intID)
-		return c.JSON(http.StatusOK, map[string]interface{}{"message": "article deleted!"})
-	})
+	// 	return c.JSON(http.StatusOK, article)
+	// })
+	// e.POST("/articles/:id", func(c echo.Context) error {
+	// 	article := Article{}
+	// 	err := c.Bind(&article)
+	// 	if err != nil {
+	// 		return err
+	// 	}
+	// 	articles[article.ID] = article
+	// 	return c.JSON(http.StatusOK, article)
+	// })
+	// e.DELETE("/articles/:id", func(c echo.Context) error {
+	// 	id := c.Param("id")
+	// 	intID, err := strconv.Atoi(id)
+	// 	if err != nil {
+	// 		return c.JSON(http.StatusBadRequest, map[string]interface{}{"message": "sorry girl, Error Response"})
+	// 	}
+	// 	delete(articles, intID)
+	// 	return c.JSON(http.StatusOK, map[string]interface{}{"message": "article deleted!"})
+	// })
 
 	e.Logger.Fatal(e.Start(":1234"))
 }
